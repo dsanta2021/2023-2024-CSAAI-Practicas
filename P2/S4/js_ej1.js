dcdbe19caa6e7625691dfb57fab5d0dc3d9b9734
David SC <d.santa.2021@alumnos.urjc.es>
2024-02-22 10:48:45 +0100

    calse jueves 22/02/24

---
+++ P2/S4/js_ej1.js
@@ -0,0 +1,31 @@
+console.log("Ejecutando js...")
+//-- Leer el display del botón 1
+const dsp1 = document.getElementById('disp1')
+
+//-- Leer los botones identificado como btX
+const btn1 = document.getElementById('bt1')
+
+const btn2 = document.getElementById('bt2')
+
+//-- Configurar el manejador para el evento de
+//-- pulsación de botón 1
+btn1.onclick = () => {
+  //console.log("Click sobre el botón 1...")
+
+  //dsp1.innerHTML = "Click sobre el botón 1..."
+  dsp1.innerHTML += " 1"
+
+  if (dsp1.style.backgroundColor == "") {
+    dsp1.style.backgroundColor = "yellow"
+  }
+
+  else{
+    dsp1.style.backgroundColor = ""
+  }
+}
+
+btn2.onclick = () => {
+  dsp1.innerHTML += " 2"
+
+  dsp1.style.backgroundColor = "red"
+}
