f8fa5a5cf228015ed5afe7ad63a59bb7ddb4557d
David SC <d.santa.2021@alumnos.urjc.es>
2024-04-12 20:18:32 +0200

    P3

---
+++ P3/parabolico.js
@@ -0,0 +1,198 @@
+// Instanciar el objeto cronómetro
+var cronometroDisplay = document.getElementById("counter-display");
+var crono = new Crono(cronometroDisplay);
+
+// Audios
+const boing_audio = new Audio('boing.mp3');
+const win_audio = new Audio('win.mp3');
+const lanzar_audio = new Audio('lanzar.mp3');
+const coin_audio = new Audio('coin.mp3');
+
+// Obtención del canvas y de los elementos HTML a usar
+const canvas = document.getElementById("canvas");
+const ball = document.getElementById("ball");
+const basket = document.getElementById("basket");
+const basketball = document.getElementById("basketball");
+const btnLanzar = document.getElementById("btnLanzar");
+const btnIniciar = document.getElementById("btnIniciar");
+const angleSldr = document.getElementById("angleSlider");
+const speedSldr = document.getElementById("speedSlider");
+const angleVal = document.getElementById("angleValue");
+const speedVal = document.getElementById("speedValue");
+const timerDisplay = document.getElementById("timer");
+
+const ctx = canvas.getContext("2d");
+
+// Canvas dimensions
+canvas.width = 900;
+canvas.height = 500;
+
+// Declaración de variables y objetos
+let xop = 50;        // Coordenadas iniciales del proyectil
+let yop = 35;
+let xp = xop;
+let yp = yop;
+let objH, objW;      // Altura y anchura del proyectil
+
+let xomin = 250;    // Coordenadas iniciales del objetivo
+let xomax = 770;
+let xo = getRandomXO(xomin, xomax); // Posición aleatoria del objetivo
+let yo = 470;
+let radius = 25;      // Radio del objetivo
+
+let velp = speedSldr.value;      // Velocidad inicial del proyectil
+let ang = angleSldr.value;    // Ángulo inicial de disparo en grados
+const grav = 9.8;       // Aceleración de la gravedad
+let time = 0;     // Tiempo de vuelo del proyectil
+
+let controlBtn = false; // Control de botón de disparo
+let win = false;  // Control de victoria
+let colision = false; // Control de colisión
+let colisionX, colisionY;   // Coordenadas de colisión
+
+
+
+// Funciones auxiliares
+function getRandomXO(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+// Función para pintar el proyectil
+function dibujarP(x, y, lx, ly) {
+    ctx.beginPath();
+    ctx.rect(x, y, lx, ly);
+    ctx.drawImage(ball, x, y, 30, 30)
+    ctx.closePath();
+
+    objH = y;
+    objW = x;
+}
+
+// Función para pintar el objetivo
+function dibujarO(x, y, img) {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.drawImage(img, x - 25, y - 50, 75, 75);
+    ctx.closePath();
+}
+
+function dibujarTiroP() {
+    xp = xop + velp * Math.cos(ang * Math.PI / 180) * time; //Se calcula posición x del proyectil
+    yp = yop + velp * Math.sin(ang * Math.PI / 180) * time - 0.5 * grav * time * time; //Se calcula posición y del proyectil
+    time += 0.1; //La simulación avanza cada 0.1 segundos, para más lento disminuir éste número
+}
+
+// Función principal de actualización
+function lanzar() {
+    // Bloquear el botón de lanzar
+    if (controlBtn == false) {
+        controlBtn = true;
+    }
+
+    // Verificación de colisión con el canvas
+    if (xp + 20 > canvas.width || xp < 0 || yp > canvas.height || yp - 20 < 0) {
+        boing_audio.currentTime = 0;
+        boing_audio.play();
+        colision = true;
+        win = false;
+    }
+    console.log(colision);
+    console.log(win);
+
+    // Verificación de colisión entre proyectil-objetivo
+    colisionX = xp + 25;
+    colisionY = (canvas.height - yp) + 25;
+    distancia = Math.sqrt((xo - colisionX) * (xo - colisionX) + (yo - colisionY) * (yo - colisionY));
+    if (distancia < radius) {
+        win_audio.currentTime = 0;
+        win_audio.play();
+        colision = true;
+        win = true;
+    }
+
+    dibujarTiroP();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+    dibujarO(xo, yo, basket);
+
+    dibujarP(xp, canvas.height - yp, 50, 50);
+
+    // Verificación de si se debe seguir el bucle
+    if (colision == false) {
+        requestAnimationFrame(lanzar);
+    }
+
+    if (colision == true && win == false) {
+        crono.stop();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.font = "110px Times New Roman";
+        ctx.fillStyle = 'red'
+        ctx.fillText("Has Perdido", 170, 250);
+    }
+    if (colision == true && win == true) {
+        crono.stop();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        dibujarO(xo, yo, basketball)
+        ctx.font = "110px Times New Roman";
+        ctx.fillStyle = 'green'
+        ctx.fillText("Has Ganado", 170, 250);
+    }
+}
+
+// Eventos
+document.onkeydown = function (ev) {
+    switch (ev.key) {
+        case " ": 
+            if (!controlBtn) {
+                crono.start();
+                lanzar();
+                controlBtn = true;
+            }
+    }
+};
+
+btnLanzar.onclick = () => {
+    if (!controlBtn) {
+        lanzar_audio.currentTime = 0;
+        lanzar_audio.play();
+        crono.start();
+        lanzar();
+        controlBtn = true;
+    }
+
+};
+
+btnIniciar.onclick = () => {
+    coin_audio.currentTime = 0;
+    coin_audio.play();
+    location.reload();
+};
+
+angleSldr.oninput = () => {
+    angulo = angleSldr.value * Math.PI / 180;
+    angleVal.textContent = angleSldr.value + '°';
+}
+
+speedSldr.oninput = () => {
+    velp = speedSldr.value;
+    speedVal.textContent = velp + ' m/s';
+}
+
+ball.onload = () => {
+    // Insertar imagen en el canvas cuando esté cargada
+    const ballWidth = 30;
+    const ballHeight = 30;
+    ctx.drawImage(ball, xop, canvas.height - yop, ballWidth, ballHeight);
+};
+
+basket.onload = () => {
+    // Insertar imagen en el canvas cuando esté cargada
+    const basketWidth = 75;
+    const basketHeight = 75;
+    ctx.drawImage(basket, xo - 25, yo - 50, basketWidth, basketHeight);
+};
+
+// Inicialización
+dibujarP(xop, yop, 50, 50); // Pintar el proyectil
+dibujarO(xo, yo, basket); // Pintar el objetivo
